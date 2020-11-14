430517fc5dcebe8874b7e8fbd05f502ff637ab09
aryelciu001 <aryelciu001@gmail.com>
2020-11-14 23:31:46 +0800

    connection to db done

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 What you need to have: 
 - JAVA SDK
 - Apache Maven 
+- MySQL
 
 How to run: 
+
+RUN MySQL Server
+1. run this mysql command: 
+
+create database cz3002;
+use cz3002;
+create table user (
+	email varchar(32),
+  password varchar(32));
+insert into user values ("admin@gmail.com", "admin");
+
+Run Struts2
 1. Go to inside the directory
-2. type "mvn jetty:run"
+2. Change MySQL credential on User.java file
+3. type "mvn jetty:run"
+
+Then, you can login using admin@gmail.com.
+
+If you insert a wrong email or password, you will be redirected back to login page with an error message.
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,17 +1,58 @@
 package org.apache.struts.assignment2.model;
+import java.sql.*;  
+import java.util.HashMap;
 
 public class User {
     private String email;
     private String password;
+    private HashMap<String, String> users;
 
     public User (String email, String password) {
         this.email = email;
         this.password = password;
+        this.users = new HashMap<String, String>();
     }
     
     public boolean isRegistered () {
-        // if user is in database
-        return true;
+        readDb();
+        String password;
+        try {
+            password = users.get(this.email);
+            // if user is in database
+            if (this.password.equals(password)) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return false;
+    }
+
+    private void readDb() {
+        String email= "";
+        String password = "";
+        try {  
+            // change this to run on your localhost
+            String userdb = "root";
+            String passworddb = "Covid1920";
+
+            String url = "jdbc:mysql://localhost:3306/cz3002?user="+userdb+"&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+            // String url = "jdbc:mysql://localhost:3306/cz3002?autoReconnect=true&useSSL=false";
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con = DriverManager.getConnection(  
+                url, userdb, passworddb);    
+            Statement stmt = con.createStatement();  
+            ResultSet rs = stmt.executeQuery("select * from user");  
+            while(rs.next()) {
+                email = rs.getString(1);
+                password = rs.getString(2);
+                users.put(email, password);
+            }
+            con.close();  
+        } catch (Exception e) { 
+            System.out.println(e);
+        }  
     }
 
     public String getEmail () {
